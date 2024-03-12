2d69be6fbbcfc304abd9466853eb61db61e96bac
gaspth64 <163174003+gaspth64@users.noreply.github.com>
2024-03-12 19:15:13 +0100

    Add files via upload

---
+++ cartes_paquet.py
@@ -0,0 +1,88 @@
+import random
+from constantes import *
+
+
+class Carte:
+    #variables de classe : correspondance hauteurs et couleurs
+    #on récupère les dessins Unicode des cartes (sauf le cavalier)
+    JEU_IMAGES = (tuple(chr(x) for x in range(0x1f0d1, 0x1f0df) if x != 0x1f0dc ),
+                tuple(chr(x) for x in range(0x1f0c1, 0x1f0cf) if x != 0x1f0cc ),
+                tuple(chr(x) for x in range(0x1f0b1, 0x1f0bf) if x != 0x1f0bc ),
+                tuple(chr(x) for x in range(0x1f0a1, 0x1f0af) if x != 0x1f0ac)
+                )
+
+
+    def __init__(self, couleur, hauteur):
+        '''
+        hauteur est un str de '1' pour l'As à 'K' pour le roi
+        couleur est un str entre 'c', 'd', 'h', 's'
+        int_hauteur est l'entier correspondant : 0 pour l'As à 12 pour le K
+        '''
+        self.couleur = couleur
+        self.hauteur = hauteur
+        self.int_hauteur = DICO_HAUTEURS[hauteur]+1
+        self.dessin = self.JEU_IMAGES[DICO_COULEURS[couleur]][DICO_HAUTEURS[hauteur]]
+        #valeur : dépend du jeu !
+        #au pharaon 1 pour l'As, 2 pour le 2, etc et 10 pour les honneurs
+        #au barbu : l'As est plus fort que le Roi, plus fort que la Dame...
+        #donc : 11 pour J, 12 pour Q
+        #13 pour K et 14 pour As
+        self.valeur = ............ # à compléter
+
+    def __str__(self):
+        #permet de faire print facilement
+        return self.couleur+self.hauteur + ' ' + self.dessin
+
+
+class PaquetCartes:
+    def __init__(self, nb):
+        self.nb_cartes = nb
+        hauteur_min = 0 if nb == 52 else 6 #on commence au 7 pour un jeu de 32
+        self.cartes = [Carte(couleur, hauteur) for hauteur in HAUTEURS[hauteur_min:]
+                                           for couleur in COULEURS]
+        if nb ==32:
+            #il faut rajouter l'As
+            self.cartes.extend([Carte(couleur, '1') for couleur in COULEURS])
+
+
+    def battre(self):
+        '''
+        mélange le paquet de façon aléatoire
+        '''
+        pass
+
+    def couper(self):
+        '''
+        simule le fait de couper le paquet en deux à un endroit aléatoire
+        puis de permuter les deux parties "dessus - dessous"
+        '''
+        pass
+
+    def est_vide(self):
+        pass
+
+    def remplir(self, cartes):
+        '''
+        cartes est une liste d'objets Carte
+        cette méthode ajoute les cartes en question au paquet
+        '''
+        pass
+
+    def tirer(self):
+        '''
+        renvoie la carte tirée au sommet du paquet
+        si le paquet est vide, lève une exception
+        '''
+        pass
+
+
+
+#tests : quand vous êtes prêt
+
+if __name__ == '__main__':
+    paq = PaquetCartes(52)
+    paq.battre()
+    for i in range(7):
+        c = paq.tirer()
+        print(c)
+    print(paq.est_vide())
---
+++ partie.py
@@ -0,0 +1,117 @@
+import random
+from cartes_paquet import Carte, PaquetCartes
+from mainjoueur import MainJoueur
+from tapis import Tapis
+from constantes import *
+
+
+
+
+
+
+## la classe principale (la plus haute) : prépare et lance la partie
+class Partie:
+    '''
+    Modélise une partie de Pharaon
+    1 joueur contre 1 IA
+    Attributs
+    @nb_joueurs : int valant 2 par défaut
+    @nb_cartes : int valant 52 par défaut
+    @nb_par_joueur : int valant 7 par défaut
+    @paquet : un objet de la classe Paquet
+    @tapis : un objet de la classe Tapis
+    @mains : liste d'objets de type MainJoueur, représentant les joueurs
+    de la Partie (et leur jeu)
+    @donneur : int représentant l'indice du joueur qui va distribuer
+    dans la liste des mains
+
+    Méthodes
+    @afficher() : fait afficher les mains des joueurs et le tapis
+    @distribuer() : remplit les mains des joueurs avec les cartes du paquet
+    @start() : prépare tout pour le début du jeu
+    ...
+
+    '''
+    def __init__(self, nb_joueurs=2, nb_cartes=52, nb_par_joueur=7):
+
+        #vérification de routine
+        if nb_joueurs < 1 or nb_joueurs > 4 or \
+        (nb_cartes != 32 and nb_cartes != 52) or \
+         nb_par_joueur > nb_cartes // nb_joueurs:
+            raise ValueError(VALUE_ERROR)
+
+
+
+        #génération du paquet de carte pour le jeu
+        self.paquet = PaquetCartes(nb_cartes)
+        self.tapis = Tapis()
+
+        self.nb_joueurs = nb_joueurs
+        self.donneur = 0
+
+        self.nb_par_joueur = nb_par_joueur
+
+        #création des mains des joueurs
+        # vides au départ !
+        #à vous
+        self.mains = .................
+
+
+    def afficher(self):
+        '''
+        affiche les jeux des joueurs
+        et le tapis
+        '''
+        pass
+
+
+
+    def distribuer(self):
+        '''
+        remplit les listes de cartes des mains pour chaque joueur
+        '''
+        servi = (self.donneur + 1) % self.nb_joueurs
+        #servi est l'indice du joueur à qui donner une carte
+        for _ in range(self.nb_par_joueur):
+            for _ in range(self.nb_joueurs):
+                #on distribue une carte à ce joueur
+                .................
+                #au prochain tour, ce sera au joueur suivant
+                servi = (servi + 1) % self.nb_joueurs
+
+
+
+
+
+    def start(self):
+        '''
+        mélange le paquet, coupe, distribue les cartes
+        retourne la première carte du talon qui vient sur la défausse
+        affiche le tout
+        puis affiche ("on démarre")
+        '''
+
+        #à vous
+
+
+
+        print("on démarre")
+
+
+
+
+#test
+if __name__ == '__main__':
+    test = Partie()
+    test.start()
+
+
+
+
+
+
+
+
+
+
+
