ff60e1a5568493658ffcb28b935f52fdd3dd6c62
gaspth64 <163174003+gaspth64@users.noreply.github.com>
2024-03-20 17:12:38 +0100

    Create tapis.py

---
+++ tapis.py
@@ -0,0 +1,32 @@
+class Tapis:
+    def __init__(self, contenu=[]):
+        self.contenu = contenu
+
+    def est_vide(self):
+        return self.contenu==[]
+
+    def empile(self,x):
+        self.contenu.append(x)
+
+    def depile(self):
+        assert self.contenu != [], "tapis vide"
+        return self.contenu.pop()
+
+    def get_premiere(self):
+        return self.contenu[-1]
+
+    def afficher(self):
+        assert self.contenu != [], "tapis vide"
+        return f'tapis : , {self.contenu[-1]}'
+
+if __name__=='__main__':
+    t = Tapis()
+    t.empile('atchoum')
+    t.empile(23)
+    t.empile([24,23])
+    t.affiche()
+    a = t.depile()
+    b = t.get_premiere()
+    t.affiche()
+
+
